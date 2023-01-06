254895dfec34a57eba92a0c882b49da98eee7efc
Vishal Choudhary <74980343+vishal-Choudhary-hi@users.noreply.github.com>
2023-01-06 12:38:42 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,6 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
-const firebaseConfig = {
-    apiKey: "AIzaSyAKE5a9I3zXuRb2AphGV_SncD5WY9_49TA",
-    authDomain: "mustic-f2dfc.firebaseapp.com",
-    projectId: "mustic-f2dfc",
-    storageBucket: "mustic-f2dfc.appspot.com",
-    messagingSenderId: "906494830290",
-    appId: "1:906494830290:web:3c936df3abf0c6a0582f0b"
-};
 
 firebase.initializeApp(firebaseConfig)
 
